4351efad4b40f7d66545145b850fb81fb4ec6028
john <sato.keita@furyu.jp>
2018-06-04 21:53:07 +0900

    タスクをリスト表示する機能を作成した

--- vue/todo.vue
+++ vue/todo.vue
@@ -1,12 +1,22 @@
 <template>
-    <h1>{{message}}</h1>
+    <main>
+        <h1>TODO</h1>
+        <ul>
+            <li v-for="task in tasks">{{task.name}}</li>
+        </ul>
+    </main>
 </template>
 
 <script>
     export default {
         data() {
             return {
-                message: 'hello world'
+                tasks: [
+                    { name: 'vueファイルを修正する', id: 1 },
+                    { name: 'タスクを追加する機能を追加する', id: 2 },
+                    { name: 'タスクを削除する機能を追加する', id: 3 },
+                    { name: 'componentを分ける', id: 4 }
+                ]
             }
         }
     }
