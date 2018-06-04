a3a452f318b738d63c18bbb398cfbab9c0f65011
john <sato.keita@furyu.jp>
2018-06-04 21:39:05 +0900

    vue.jsのインストール

---
+++ entry.js
@@ -0,0 +1,4 @@
+import Vue from 'Vue';
+import ToDo from './vue/todo.vue';
+
+new Vue(ToDo).$mount(document.body)
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,19 @@
 const path = require('path')
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
 
 module.exports = {
     entry: path.resolve(__dirname, "entry.js"),
+    plugins: [
+        new VueLoaderPlugin()
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                exclude: /node_modules/,
+                use: 'vue-loader'
+            }
+        ]
+    }
 }
