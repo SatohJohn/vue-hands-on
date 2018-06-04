42263bf4a6cefe3aea8822b0ae5ecc5b5f41b3ba
john <sato.keita@furyu.jp>
2018-06-04 22:02:16 +0900

    タスクを追加する機能を作成した

--- vue/todo.vue
+++ vue/todo.vue
@@ -4,6 +4,8 @@
         <ul>
             <li v-for="task in tasks">{{task.name}}</li>
         </ul>
+        <input type="text" v-model="taskName">
+        <button @click="addTask">タスクを追加する</button>
     </main>
 </template>
 
@@ -16,7 +18,18 @@
                     { name: 'タスクを追加する機能を追加する', id: 2 },
                     { name: 'タスクを削除する機能を追加する', id: 3 },
                     { name: 'componentを分ける', id: 4 }
-                ]
+                ],
+                taskName: '',
+                nextId: 5
+            }
+        },
+        methods: {
+            addTask() {
+                this.tasks.push({
+                    name: this.taskName,
+                    id: this.nextId++
+                });
+                this.taskName = '';
             }
         }
     }
